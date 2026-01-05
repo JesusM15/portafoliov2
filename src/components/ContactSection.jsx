2e2bdf6747437f09dbe6625793afcbdbf10cf345
JesusM15 <guerrajesusm72@gmail.com>
2026-01-05 10:45:03 -0800

    feat: formData integration email sender accept

--- src/components/ContactSection.jsx
+++ src/components/ContactSection.jsx
@@ -11,6 +11,25 @@ import { FiPhone } from "react-icons/fi";
 
 export default function ContactSection(){
     const { t } = useTranslation();
+    const [ result, setResult ] = useState();
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const company = formData.get('company');
+
+        if(company){
+            formData.set('name', `${formData.get('name')} - ${company}`);
+        }
+
+        const response = await fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            body: formData
+        });
+
+        const data = await response.json();
+        setResult(data.success  ? "Success" : "Error");
+    }
     
     return <section className=" soft-background py-24 px-4  text-center" id="contact_me">
         <h3 className="text-5xl font-bold pb-2">
@@ -21,21 +40,26 @@ export default function ContactSection(){
         </p>
 
         <section className="flex gap-4 px-4 pt-6">
-            <form className="flex-1 flex flex-col gap-4 bg-slate-900/80 rounded-sm border border-slate-700 p-6 pt-8 w-full">
+            <form 
+            onSubmit={onSubmit}
+            className="flex-1 flex flex-col gap-4 bg-slate-900/80 rounded-sm border border-slate-700 p-6 pt-8 w-full">
                 <Input 
                     label={'Nombre'}   
                     required
+                    name="name"
                     placeholder={'Escribe tu nombre aquí'}
                 />
                 <Input 
                     label={'Email'} 
                     type={'email'}  
                     required
+                    name="email"
                     placeholder={'example@gmail.com'}
                 />
                 <Input 
                     label={'Empresa/Organización'} 
                     placeholder={'Nombre de tu empresa (opcional)'}
+                    name="company"
                 />
 
                 <div className="flex flex-col gap-1 items-start text-slate-300 text-base ">
@@ -44,6 +68,7 @@ export default function ContactSection(){
             focus:border-purple-600 focus:ring-purple-600 outline-0 flex-1 w-full
             border opacity-80 rounded-md min-h-24  bg-slate-800 border-slate-700 shadow-sm text-slate-300 resize-none"
                     placeholder="Escribe tu mensaje aquí"
+                     name="message"
             >
 
                     </textarea>
@@ -51,7 +76,7 @@ export default function ContactSection(){
 
                 <button className="
                 flex items-center justify-center gap-2 text-md cursor-pointer outline-0
-                bg-gradient-to-r from-rose-500 via-purple-500 to-purple-600 px-2 py-2.5 rounded-md ">
+                bg-gradient-to-r from-rose-500 via-purple-500 to-purple-600 px-2 py-2.5 rounded-md " type="submit">
                     <IoIosSend className="text-2xl" />
                     Enviar mensaje
                 </button>
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-export default function Input({ onChange, label, placeholder=label, type = 'text', required=false  }){
+export default function Input({ onChange, label, placeholder=label, type = 'text', required=false, name, value }){
     return <div className="flex flex-col gap-1 items-start text-slate-300 text-base ">
         <label>{label}{required && ' *'}</label>
         <input 
@@ -8,8 +8,10 @@ export default function Input({ onChange, label, placeholder=label, type = 'text
             focus:border-purple-600 focus:ring-purple-600 outline-0 flex-1 w-full
             border opacity-80 rounded-md  bg-slate-800 border-slate-700 shadow-sm text-slate-300"
             type={type}
+            name={name}
             required={required}
             placeholder={placeholder}
+            onChange={onChange}
         />
     </div>
 }
