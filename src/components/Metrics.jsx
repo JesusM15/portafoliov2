5a1e92ec08182c7494a3b3d3502e52259361ed15
= <=>
2026-04-26 19:37:36 -0700

    hotfix

--- src/components/Metrics.jsx
+++ src/components/Metrics.jsx
@@ -6,11 +6,11 @@ export default function Metrics({
     number,
     plus = true,
     label,
-}){
+}) {
 
     return <div>
         <b className="flex items-center text-xl md:text-3xl font-bold">
-            {number} <FaPlus className="md:text-xl text-sm" />
+            {number} {plus && <FaPlus className="md:text-xl text-sm" />}
         </b>
         <p className="text-xs md:text-sm opacity-80 text-gray-100">
             {label}
